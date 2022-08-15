fe19f591e832a308d0ff531b679e735c7e90decd
David Bonser <Davidbonzer@hotmail.co.uk>
2022-08-15 14:10:14 +0100

    add funtionality added - yet not working yet

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -29,4 +29,8 @@ export class TasksComponent implements OnInit {
     this.taskService.updateTaskReminder(task);
   }
   
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
+  
 }
