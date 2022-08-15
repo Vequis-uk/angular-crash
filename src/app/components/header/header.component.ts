25cf4b594d3b1841a1ad6f3e89848b59a49c0bc5
David Bonser <Davidbonzer@hotmail.co.uk>
2022-08-15 15:28:31 +0100

    added toggle for add button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +9,14 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask!: boolean;
+  subscription!: Subscription;
 
-  constructor() { }
+  constructor(private uiService:UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe(value => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {}
 
